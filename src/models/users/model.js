90fb7d62507e4781919ed8670e603fb249c99de3
Edward-Regalado <tregalado30@gmail.com>
2022-08-06 14:04:49 -0700

    added user model and user collection

---
+++ src/models/users/model.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+
+const userModel = (sequelize, DataTypes) => {
+    const model = sequelize.define('User', {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        role: {
+            type: DataTypes.STRING,
+        },
+        token: {
+            type: DataTypes.VIRTUAL,
+            get(){
+                const payload = { username: this.username, role: this.role };
+                return jwt.sign(payload, SECRET);
+                },
+            },
+        });
+    return model;
+};
+
+module.exports = userModel;
