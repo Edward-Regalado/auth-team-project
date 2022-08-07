7e94f04549cbcf8f51d59d71499858edfa3fae50
Edward-Regalado <tregalado30@gmail.com>
2022-08-06 18:18:23 -0700

    fixed validate token error

--- src/middleware/auth/validateToken.js
+++ src/middleware/auth/validateToken.js
@@ -2,20 +2,45 @@ require("dotenv").config();
 const SECRET = process.env.SECRET;
 const jwt = require("jsonwebtoken");
 
-function validateToken(req, res, next) {
-  try {
-    const token = req.headers["authorization"];
-    if (!token) {
-      res.status(401).send("unauthorized");
+// function validateToken(req, res, next) {
+//   try {
+//     const token = req.headers["authorization"];
+//     if (!token) {
+//       res.status(401).send("unauthorized");
+//       next();
+//     } else {
+//       const user = jwt.verify(token, SECRET);
+//       req.user = user;
+//       next();
+//     }
+//   } catch (e) {
+//     console.log(`error message: ${e.message}`);
+//   }
+// }
+
+const validateToken = async (req, res, next ) => {
+      // const username = req.body.username;
+      try {
+          const authHeader = req.headers['authorization'];
+          const token = authHeader && authHeader.split(' ')[1];
+          if (token === null) {
+              return res.status(401).send("You are not authorized!");
+          }
+          jwt.verify(token, SECRET, (error, user) => {
+              if(error) {
+                  return res.status(403);
+              }
+              req.user = user;
+              console.log('/////////////////////////////////////////////////////');
+              console.log(`user: ${user}`);
+              console.log(`req.user: ${req.user}`);
+              next();
+          });
+  
+      } catch (error) {
+          throw new Error(error);
+      }
       next();
-    } else {
-      const user = jwt.verify(token, SECRET);
-      req.user = user;
-      next();
-    }
-  } catch (e) {
-    console.log(`error message: ${e.message}`);
-  }
-}
+  };
 
 module.exports = validateToken;
